8f8747d3e8bef6ff1d92e78ddcf7d016f59307ff
Artur Comoli <artur.comoli@gmail.com>
2022-08-25 22:27:01 -0300

    feat: heroku config
    Signed-off-by: Artur Comoli <artur.comoli@gmail.com>

--- api/base_project/settings.py
+++ api/base_project/settings.py
@@ -63,7 +63,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PART_APPS + MY_APPS
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    # "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
